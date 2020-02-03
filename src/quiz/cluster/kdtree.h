07b3f471aa05ee835d21becee4c6fb3043923179
Ravichandra Malapati <malapatiravi@gmail.com>
2020-02-02 23:02:28 -0500

    Implemented KD search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -53,11 +53,58 @@ struct KdTree
 		// the function should create a new node and place correctly with in the root
 		insertRecHelper(&root, point, id, 0);
 	}
+	bool isInBox(std::vector<float> target, std::vector<float> point, float distanceTol)
+	{
+		bool inBox = true;
+		for(int i = 0; i < target.size(); i++)
+		{
+			inBox = inBox && (point[i] <= (target[i] + distanceTol)) && (point[i] >= (target[i]-distanceTol));
+		}
+		return inBox;
+	}
+	bool isInDistance(std::vector<float> target, std::vector<float> point, float distanceTol)
+	{
+		float x_dist = point[0]-target[0];
+		float y_dist = point[1]-target[1];
+		float z_dist = point[2]-target[2];
 
+		return ((x_dist*x_dist)+ (y_dist*y_dist) + (z_dist*z_dist) ) <= (distanceTol*distanceTol);
+
+
+	}
+	void searchRecHelper(std::vector<float> target, float distanceTol, Node* node, int depth, std::vector<int>& ids)
+	{
+		if(node != NULL)
+		{
+			if(isInBox(target, node->point, distanceTol))
+			{
+				if(isInDistance(target, node->point, distanceTol))
+				{
+					ids.push_back(node->id);
+				}
+			}
+
+			if((target[depth%2] - distanceTol) < node->point[depth%2])
+			{
+				searchRecHelper(target, distanceTol, node->left, depth+1, ids);
+			}
+			if((target[depth%2] + distanceTol) > node->point[depth%2])
+			{
+				searchRecHelper(target, distanceTol, node->right, depth+1, ids);
+			}
+		}
+		else
+		{
+			return;
+		}
+		
+	}
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+
+
 		return ids;
 	}
 };
